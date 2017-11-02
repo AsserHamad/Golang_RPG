b19a580d2ac1fd509bd8cef8830cccbc54120319
Omar Hossam <moar.ahmed@gmail.com>
2017-11-02 23:51:39 +0200

    Add DB setup in a seperate file, and added new model registration

--- routers/router.go
+++ routers/router.go
@@ -2,18 +2,13 @@ package routers
 
 import (
 	"Golang_RPG/controllers"
-	"Golang_RPG/models"
 
 	"github.com/astaxie/beego"
-	"github.com/astaxie/beego/orm"
 )
 
 func init() {
 	// TODO: put initializations in a differnt file
-	orm.RegisterDriver("mysql", orm.DRMySQL)
-	// TODO: add connection string in app.con and unify it on our systems
-	orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("connectionString"))
-	orm.RegisterModel(new(models.Users))
+
 	beego.Router("/api/", &controllers.MainController{})
 	beego.Router("/api/register", &controllers.RegisterController{})
 	beego.Router("/api/login", &controllers.LoginController{})
